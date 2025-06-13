7d7c19e39a4621685d3a4d1d7eb98d15ed811355
Sangeetha Sankar <59412646+sangeetha-sankar@users.noreply.github.com>
2025-06-13 22:39:17 +0530

    Create use-inputs.js

---
+++ src/hooks/use-inputs.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const useInput = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && isTouched;
+
+  const valueChangeHandler = (e) => {
+    setEnteredValue(e.target.value);
+
+    // console.log(e.target.id);
+  };
+
+  const inputBlurHander = (e) => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredValue("");
+    setIsTouched(false);
+  };
+
+  return {
+    value: enteredValue,
+    hasError,
+    valueChangeHandler,
+    inputBlurHander,
+    isValid: valueIsValid,
+    reset,
+  };
+};
+
+export default useInput;
